9929b3b57919e9e9cd874995d7c870364468f095
emi.dev <136280309+emivecampos@users.noreply.github.com>
2026-05-25 16:57:07 -0300

    Codigo mais limpo

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
 let heroi = "Welton";
-let xpHeroi = 4700; 
-
+let xpHeroi = 4700;
+let nivel = "";
 
 switch (true) {
     case (xpHeroi <= 1000):
-    console.log("O heroi" + heroi + "é iniciante");
-    break;
-}
-
-switch (true) {
+        nivel = "iniciante";
+        break;
     case (xpHeroi >= 1001 && xpHeroi <= 2000):
-    console.log("O heroi" + heroi + "é bronze");
-    break;
-}
-
-switch (true) {
+        nivel = "bronze";
+        break;
     case (xpHeroi >= 2001 && xpHeroi <= 5000):
-    console.log("O heroi" + heroi + "é prata");
-    break;
-}
-
-switch (true) {
+        nivel = "prata";
+        break;
     case (xpHeroi >= 5001 && xpHeroi <= 7000):
-    console.log("O heroi" + heroi + "é ouro");
-    break;
-}
-
-switch (true) {
+        nivel = "ouro";
+        break;
     case (xpHeroi >= 7001 && xpHeroi <= 8000):
-    console.log("O heroi" + heroi + "é platina");
-    break;
-}
-
-switch (true) {
+        nivel = "platina";
+        break;
     case (xpHeroi >= 8001 && xpHeroi <= 9000):
-    console.log("O heroi" + heroi + "é ascendente");
-    break;
-}
-
-switch (true) {
+        nivel = "ascendente";
+        break;
     case (xpHeroi >= 9001 && xpHeroi <= 10000):
-    console.log("O heroi" + heroi + "é imortal");
-    break;
-}
-
-switch (true) {
+        nivel = "imortal";
+        break;
     case (xpHeroi >= 10001):
-    console.log("O heroi" + heroi + "é radiante");
-    break;
+        nivel = "radiante";
+        break;
 }
 
-console.log("O heroi"  +  heroi +  "tem"  + xpHeroi  +  "de xp");
+console.log("O heroi " + heroi + " é " + nivel + " com " + xpHeroi + " pontos de experiência! ");
